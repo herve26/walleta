54d494d97b5e12ed032ac25116de00c222dc4676
herve26 <nzanzuh@gmail.com>
2020-12-10 19:31:54 +0100

    setup the grid Added the three columns layout as per our design

--- src/Accounts/AccountsSideBar.js
+++ src/Accounts/AccountsSideBar.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.aside`
   border: 1px solid blue;
+  width: 250px;
 `;
 export default function AccountsSideBar() {
   return <Container>Accounts</Container>;
--- src/Main/MainView.js
+++ src/Main/MainView.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 
 const Container = styled.main`
   border: 1px solid blue;
-`;
+  flex-grow: 1;
+`
+
 export default function MainView() {
   return <Container>Main</Container>;
 }
