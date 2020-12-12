f7a88bc24ed81f5a8a096ffdbd969c2253f1a928
herve26 <nzanzuh@gmail.com>
2020-12-12 09:28:51 +0100

    added the add icon

--- src/Accounts/NewAccount.js
+++ src/Accounts/NewAccount.js
@@ -1,31 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
 
 import { ButtonContainer as Container, IconContainer } from './stylesComponent';
 
-// const Container = styled.div`
-//     border: 2px solid ${props => props.colour};
-//     display: flex;
-//     align-items: center;
-//     border-radius: 12px;
-//     height: 3.75rem;
-//     padding: 8px;
-// `
-// const Icon = styled.div`
-//     background-color: ${props => props.active ? 'white' : props.colour};
-//     color: ${({active, colour}) => active ? colour : 'white'};
-//     height: 36px;
-//     width: 36px;
-//     border-radius: 50%;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     margin-right: 8px;
-// `
-
 export default function NewAccount(){
+    const [isOpen, setOpen] = useState(true)
     return(
-        <Container>
-            <IconContainer colour={'black'}>W</IconContainer>
+        <Container onClick={() => setOpen(!isOpen)}>
+            <IconContainer colour={'black'}><AddIcon/></IconContainer>
             Add a New Account
         </Container>
     )
