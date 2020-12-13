4b08fa761f2efe4d4a0a87ffeb79179264fd1000
herve26 <nzanzuh@gmail.com>
2020-12-13 18:41:31 +0100

    Added new Account Form

--- src/Accounts/NewAccount.js
+++ src/Accounts/NewAccount.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import AddIcon from '@material-ui/icons/Add';
 
+import AddAccountForm from './AddAccountForm';
+
 import { ButtonContainer as Container, IconContainer } from './stylesComponent';
 
 export default function NewAccount(){
-    const [isOpen, setOpen] = useState(true)
+    const [isOpen, setOpen] = useState(false)
+    const closeButton = () => {
+        console.log('The Mark')
+        setOpen(false)
+    }
     return(
-        <Container onClick={() => setOpen(!isOpen)}>
-            <IconContainer colour={'black'}><AddIcon/></IconContainer>
-            Add a New Account
-        </Container>
+        <Container onClick={() => setOpen(true)}>
+            {!isOpen ?
+            <>
+                <IconContainer colour={'black'}><AddIcon/></IconContainer>
+                Add a New Account
+            </> :
+            <AddAccountForm close={closeButton}/>}
+        </Container> 
     )
 }
