104c29b4eb2a5742647b1b517c13fc89a0cde2f9
herve26 <nzanzuh@gmail.com>
2020-12-15 17:29:07 +0100

    Created a custom Field component for drop down element selection

---
+++ src/Accounts/FieldCustom.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styled, {css} from 'styled-components';
+
+const Container = styled.div`
+	/*border: 1px solid red;*/
+`
+const CurrentElmContainer = styled.div`
+	/*border: 1px solid red;*/
+    padding: 8px 16px;
+    background-color: #F3F8FB;
+    border-radius: 12px;
+    margin-bottom: 8px;
+    ${({isOpen}) => isOpen && css`
+    	margin-bottom: 0px;
+    	border-radius: 12px 12px 0 0;
+    	border-bottom: 1px solid #00000029;
+    `}
+    font-size: 24px;
+    height: 36px; 
+    display:flex;
+    align-items: center;
+    &:after{
+    	border: solid black;
+    	border-width: 0 3px 3px 0;
+    	padding: 3px;
+		right: 36px;
+		content: "";
+		position: absolute;
+		pointer-events: none;
+		transform: rotate(45deg)
+
+    }
+`
+const ElementsContainer = styled.div`
+	/*border: 1px solid red;*/
+	display: flex;
+	padding: 8px;
+	background-color: #F3F8FB;
+	flex-wrap: wrap;
+	border-radius: 0 0 12px 12px;
+	justify-content: space-between;
+	margin-bottom: 8px;
+	/*display:none;*/
+	${({isOpen}) => !isOpen && css`
+		height: 0px;
+		width: 0px;
+		overflow: hidden;
+		padding: 0px;
+		margin-bottom: 0px;
+	`}
+
+	transition: height 2s ease-out;
+`
+const ElmContainer = styled.span`
+	/*border: 1px solid blue;*/
+
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 24px;
+	background-color: #FFFFFF;
+	border-radius: 8px;
+	margin-right: 8px;
+	margin-bottom: 8px;
+	height: 36px;
+	width: 36px;
+	cursor: pointer;
+	${({current}) => current ? 'box-shadow: 0px 3px 6px #00000029' : ''};
+	&:hover{
+		box-shadow: 0px 3px 6px #00000029;
+	}
+
+`
+
+export default function FieldCustom({name, value, elements, tabindex, onChanged}){
+	const [isOpen, setOpen] = useState(false)
+	const [currentElmIdx, setCurrentElmIdx] = useState(0)
+	useEffect(() => {
+		onChanged(currentElmIdx)
+	}, [currentElmIdx])
+	const elementsList = elements.map((Elm, index) => 
+				<ElmContainer 
+					current={currentElmIdx=== index} 
+					key={index} 
+					onClick={() => {setCurrentElmIdx(index); setOpen(false);}}
+				>
+					{Elm}
+				</ElmContainer>)
+	const CurrentElm = elements[currentElmIdx]
+	const handleKeypress = e => {
+		if(e.key === " " || e.key === 'Enter')
+			setOpen(!isOpen)
+		if(e.key === 'ArrowLeft' && isOpen){
+			const sel = currentElmIdx - 1
+			setCurrentElmIdx(sel >= 0 ? sel : elements.length - 1)
+		}
+		if(e.key === 'ArrowRight' && isOpen){
+			const sel = currentElmIdx + 1
+			setCurrentElmIdx(sel < elements.length ? sel : 0)
+		}
+	}
+	return(
+		<Container>
+			<input value={value} name={name} type='hidden'/> 
+			<CurrentElmContainer 
+				tabIndex={tabindex || "0"}
+				isOpen={isOpen} 
+				onClick={() => setOpen(!isOpen)}
+				onKeyDown={e => handleKeypress(e)}
+			>
+				{CurrentElm}
+			</CurrentElmContainer>
+			<ElementsContainer isOpen={isOpen}>{elementsList}</ElementsContainer>
+		</Container>
+	)
+}
+
+FieldCustom.propTypes = {
+	elements: PropTypes.array.isRequired,
+	tabindex: PropTypes.string,
+	onChanged: PropTypes.func.isRequired
+}
---
+++ src/Accounts/FieldCustom.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import FieldCustom from './FieldCustom';
+import accountColors from '../colors.json';
+
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+
+const icons = [AccountBalanceWalletIcon, PhoneAndroidIcon,
+			AccountBalanceWalletIcon, PhoneAndroidIcon,
+			AccountBalanceWalletIcon, PhoneAndroidIcon,
+			AccountBalanceWalletIcon, PhoneAndroidIcon,
+			AccountBalanceWalletIcon, PhoneAndroidIcon]
+
+export default {
+	title: 'Account Panel/New Account Button/Add Account Form/Field Colors',
+	component: FieldCustom,
+	argTypes: { onChanged: { action: 'Field Changed'}}
+}
+
+const Template = args => <FieldCustom {...args}/>
+export const Default = Template.bind({})
+Default.args = {
+	elements: []
+}
+
+const colorsList = accountColors.map((color, index) => <div key={index} style={{background: `${color}`, height: '100%', width: '100%', borderRadius: 8}}></div>)
+export const Colors = Template.bind({})
+Colors.args = {
+	elements: colorsList
+}
+
+const iconsList = icons.map((Icon, index) => <Icon key={index} fontSize='inherit'/>)
+export const Icons = Template.bind({})
+Icons.args = {
+	elements: iconsList
+}
