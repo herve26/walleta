5b5f07ac811ba679ad095fb437c967cba93db4b5
herve26 <nzanzuh@gmail.com>
2020-12-16 02:48:57 +0100

    created icons file for icons export

---
+++ src/icons.js
@@ -0,0 +1,7 @@
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+
+export default [
+	AccountBalanceWalletIcon,
+	PhoneAndroidIcon
+]
