02ba66af778a85dad833ee25ef780ffeb07010e4
herve26 <nzanzuh@gmail.com>
2020-12-18 15:07:07 +0100

    Modified middleware to generate ids with uuid

--- src/Redux/middleware.js
+++ src/Redux/middleware.js
@@ -1,10 +1,15 @@
 import { accountSlice } from './store';
+import { v4 as uuid } from 'uuid';
 // const { add_account } = accountSlice.actions;
 
 export const identifyAccount = store => next => action => {
 	const { add_account } = accountSlice.actions
-	if (action.type === add_account.toString()){
-		action.payload = {...action.payload, id: store.getState().accounts.length}
+	const id = uuid()
+	console.log(id)
+
+	if (action.type === add_account.toString() && !store.getState().accounts[id]){
+
+		action.payload = {...action.payload, id: id}
 	}
 	return next(action)
 }
