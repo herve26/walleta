0a72a0f380707634298b56a81e70b775c839e0f0
herve26 <nzanzuh@gmail.com>
2020-12-18 15:15:21 +0100

    styled the Aside panel

--- src/Aside/TransactionsInput.js
+++ src/Aside/TransactionsInput.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.aside`
-  border: 1px solid blue;
-  width: 250px;
-`;
+import { PanelContainer as Container } from '../globalStyle';
+
+// const Container = styled.aside`
+//   border: 1px solid blue;
+//   width: 250px;
+// `;
 
 export default function TransactionsInput() {
   return <Container>Aside</Container>;
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from "styled-components";
+import styled, {createGlobalStyle} from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   html {
@@ -9,3 +9,12 @@ export const GlobalStyle = createGlobalStyle`
   }
 
 `
+
+export const PanelContainer = styled.aside`
+  width: 250px;
+  padding: 24px;
+  border-radius: 24px;
+  box-shadow: 0px 3px 6px #00000029;
+  height: 100%;
+  background: white;
+`;
